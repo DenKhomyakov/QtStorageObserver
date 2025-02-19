374f8cfd6441dbd356f6817fb593033c06d7f85c
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-02-19 18:27:23 +0700

    All methods of the DataModel class are implemented

--- StorageObserverGUI/datamodel.cpp
+++ StorageObserverGUI/datamodel.cpp
@@ -15,3 +15,51 @@ int DataModel::columnCount(const QModelIndex& parent) const {
 
     return dataModel.count() + 1;
 }
+
+QVariant DataModel::data(const QModelIndex& index, int role) const {
+    if (index.isValid() || dataModel.count() <= index.row() || (role != Qt::DisplayRole && role != Qt::EditRole)) {
+        return QVariant();
+    }
+
+    switch (index.column()) {
+    case NAME:
+        return dataModel[index.row()].name;
+        break;
+
+    case SIZE:
+        return dataModel[index.row()].size;
+        break;
+
+    case PERCENT:
+        return dataModel[index.row()].percent;
+        break;
+
+    default:
+        break;
+    }
+
+    return QVariant();
+}
+
+QVariant DataModel::headerData(int section, Qt::Orientation orientation, int role) const {
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (section) {
+        case NAME:
+            return "Name";
+
+        case SIZE:
+            return "Size";
+
+        case PERCENT:
+            return "Percent";
+        }
+    }
+
+    return QVariant();
+}
+
+void DataModel::setModel(QList<TableModel> model) {
+    dataModel = model;
+
+    emit layoutChanged();
+}
