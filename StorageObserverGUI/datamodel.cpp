9bb27c2b3fe18ac194eba3864a3d79af771333bf
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-02-19 16:58:11 +0700

    Several methods of the DataModel class have been implemented

--- StorageObserverGUI/datamodel.cpp
+++ StorageObserverGUI/datamodel.cpp
@@ -1,1 +1,17 @@
 #include "datamodel.h"
+
+DataModel::DataModel(QObject* parent, QList<TableModel> table) : QAbstractTableModel(parent) {
+    dataModel = table;
+}
+
+int DataModel::rowCount(const QModelIndex& parent) const {
+    Q_UNUSED(parent);
+
+    return dataModel.count();
+}
+
+int DataModel::columnCount(const QModelIndex& parent) const {
+    Q_UNUSED(parent);
+
+    return dataModel.count() + 1;
+}
