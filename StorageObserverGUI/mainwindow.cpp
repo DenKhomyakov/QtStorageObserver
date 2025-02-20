4605cf10e34529020f5be9acfb253d9707e704d6
Denis Khomyakov <Homyakov.Denis2021@yandex.ru>
2025-02-20 22:42:38 +0700

    Implemented the constructor of the MainWindow class, fixed syntax errors in the selectedSlot and strategyBoxSlot methods

--- StorageObserverGUI/mainwindow.cpp
+++ StorageObserverGUI/mainwindow.cpp
@@ -2,7 +2,46 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget* parent) : QWidget(parent) {
-    // TODO
+    setWindowTitle("Storage Observer");
+    this->strategy = new Context(new ByFolderCalculationStrategy);
+    model = new DataModel(this);
+
+    systemModel = new QFileSystemModel(this);
+    systemModel->setFilter(QDir::NoDotAndDotDot | QDir::AllDirs);
+    systemModel->setRootPath(QDir::homePath());
+
+    treeView = new QTreeView(this);
+    treeView->setModel(systemModel);
+
+    tableView = new QTableView(this);
+    tableView->setModel(model);
+
+    strategyLabel = new QLabel("Select the typy of the review", this);
+    strategyBox = new QComboBox(this);
+    strategyBox->addItems({"By folder", "By file type"});
+
+    panelLayout = new QHBoxLayout();
+    if (panelLayout != nullptr) {
+        panelLayout->addStretch(1);
+        panelLayout->addWidget(strategyLabel);
+        panelLayout->addWidget(strategyBox);
+    }
+
+    viewsLayout = new QHBoxLayout();
+    if (viewsLayout != nullptr) {
+        viewsLayout->addWidget(treeView);
+        viewsLayout->addWidget(tableView);
+        viewsLayout->setStretchFactor(tableView, 1);
+    }
+
+    mainLayout = new QVBoxLayout(this);
+    if (mainLayout != nullptr) {
+        mainLayout->addLayout(panelLayout);
+        mainLayout->addLayout(viewsLayout);
+    }
+
+    connect(treeView->selectionModel(), &QItemSelectionModel::selectionChanged, this, &MainWindow::selectedSlot);
+    connect(strategyBox, qOverload<int>(&QComboBox::currentIndexChanged), this, &MainWindow::strategyBoxSlot);
 }
 
 MainWindow::~MainWindow() {
@@ -53,7 +92,7 @@ void MainWindow::selectedSlot(const QItemSelection& selected, const QItemSelecti
     QModelIndex index = treeView->selectionModel()->currentIndex();
 
     if (index.isValid()) {
-        model.setModel(fillTable(strategy->calculationMethod(systemModel->filePath(index))));
+        model->setModel(fillTable(strategy->calculationMethod(systemModel->filePath(index))));
     }
 }
 
@@ -69,5 +108,5 @@ void MainWindow::strategyBoxSlot(int strategy) {
     }
 
     QModelIndex index = treeView->selectionModel()->currentIndex();
-    model.setModel(fillTable(this->strategy->calculationMethod(systemModel->filePath(index))));
+    model->setModel(fillTable(this->strategy->calculationMethod(systemModel->filePath(index))));
 }
--- StorageObserverGUI/mainwindow.h
+++ StorageObserverGUI/mainwindow.h
@@ -20,7 +20,7 @@ public:
     ~MainWindow();
 
 private:
-    DataModel model;
+    DataModel* model;
     QTableView* tableView;
     QFileSystemModel* systemModel;
     QTreeView* treeView;
